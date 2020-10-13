4e7e2542baced9dd16f86bc9fd2178bc8fd3e0ee
juanalberto58 <juanalberto580@gmail.com>
2020-10-13 20:35:45 +0200

    Actualizo código para Hito1,closes #9

--- src/YourLife.go
+++ src/YourLife.go
@@ -14,7 +14,7 @@ func main() {
 	e.Titulo = "\nPrimera entrada del diario\n"
 	e.Dia = "13/10/2020\n"
 	e.Hora = "19:00\n"
-	e. Texto = "Escribo la primera entrada del diario\n"
+	e. Texto = "Escribo la primera entrada del diario.\n"
 
     fmt.Println(e)
 }
