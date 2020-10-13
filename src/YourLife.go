99b95354f8fd30bada816f6d750d09925f6c4c60
juanalberto58 <juanalberto580@gmail.com>
2020-10-13 20:21:56 +0200

    Actualizo codigo para Hito 1

--- src/YourLife.go
+++ src/YourLife.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+type Entrada struct{
+	Titulo string
+	Dia string 
+	Hora string
+	Texto string
+}
+
 func main() {
-    fmt.Println("Comienzo de estructura para el proyecto de IV")
+	var e Entrada
+	e.Titulo = "\nPrimera entrada del diario\n"
+	e.Dia = "13/10/2020\n"
+	e.Hora = "19:00\n"
+	e. Texto = "Escribo la primera entrada del diario\n"
+
+    fmt.Println(e)
 }
 
 
