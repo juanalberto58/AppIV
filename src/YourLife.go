f0ea1ea3a1b9178ff2715a28cdf78e9ce46130d3
juanalberto58 <juanalberto580@gmail.com>
2020-10-14 20:10:21 +0200

    Creando la primera historia de usuario en código,closes #4

--- src/YourLife.go
+++ src/YourLife.go
@@ -9,14 +9,45 @@ type Entrada struct{
 	Texto string
 }
 
-func main() {
-	var e Entrada
-	e.Titulo = "\nPrimera entrada del diario\n"
-	e.Dia = "13/10/2020\n"
-	e.Hora = "19:00\n"
-	e. Texto = "Escribo la primera entrada del diario.\n"
+func GuardarEntrada(tit string, d string, h string, text string){
+	var n Entrada
+	n.Titulo = tit
+	n.Dia = d
+	n.Hora = h
+	n.Texto = text
+}
 
-    fmt.Println(e)
+func main() {
+
+	var e1 Entrada
+
+	menu := `
+	Bienvenido a YourLife
+	[ 1 ] Saludo
+	[ 2 ] Introducir una entrada
+	[ 3 ] Leer una entrada
+    `
+	fmt.Print(menu)
+
+	var opcion int 
+	fmt.Scanln(&opcion)
+
+	switch opcion{
+			case 1:
+				fmt.Println("Bienvenido a YourLife")
+			case 2:
+				fmt.Println("Ponle titulo a tu dia")
+				fmt.Scanln(&e1.Titulo)
+				fmt.Println("¿Que dia es hoy?")
+				fmt.Scanln(&e1.Dia)
+				fmt.Println("¿Que hora es hoy?")
+				fmt.Scanln(&e1.Hora)
+				fmt.Println("Cuentanos que te ha ocurrido hoy")
+				fmt.Scanln(&e1.Texto)
+	}
+
+	GuardarEntrada(e1.Titulo,e1.Dia,e1.Hora,e1.Texto)
+
 }
 
 
