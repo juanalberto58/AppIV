8cde62cc15cece698abdd801cb854942d8f39183
juanalberto58 <juanalberto580@gmail.com>
2020-12-29 18:11:17 +0100

    Subiendo api

---
+++ api/diario.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/juanalberto58/AppIV/src/YourLife"
+  	"strings"
+)
+
+var diario = []Entrada {
+	Entrada {
+		titulo: "Me voy de pesca",
+		dia: "Lunes",
+		hora: "17:00",
+		entrada: "Hoy me he ido de pesca al rio"
+	},
+	Entrada {
+		titulo: "Me voy de caza",
+		dia: "Martes",
+		hora: "11:00",
+		entrada: "Hoy me he ido de caza al monte"
+	},
+	Entrada {
+		titulo: "Me voy a comer a un restaurante",
+		dia: "Miercoles",
+		hora: "14:00",
+		entrada: "Hoy me he ido a comer a un restaurante"
+	},
+	Entrada {
+		titulo: "Me voy a Sevilla",
+		dia: "Jueves",
+		hora: "10:00",
+		entrada: "Hoy me he ido a Sevilla"
+	},
+	Entrada {
+		titulo: "Me voy a Jaen",
+		dia: "Viernes",
+		hora: "11:00",
+		entrada: "Hoy me he ido a Jaen"
+	},
+
+}
+
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	
+	
+}
