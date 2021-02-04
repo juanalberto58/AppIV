268674582893a7b74cec57b37430394f5360ae60
juanalberto58 <you@example.com>
2021-02-04 12:58:53 +0100

    Actualizando micro

--- src/main.go
+++ src/main.go
@@ -7,6 +7,35 @@ import(
 
 var new m.Entrada
 
+func introducirEntrada(c *gin.Context){
+	
+	titulo := c.PostForm("titulo")
+	dia := c.PostForm("dia")
+	hora := c.PostForm("hora")
+	entrada := c.PostForm("entrada")
+
+	new.GuardarEntrada(titulo,dia,hora,entrada)
+
+	c.JSON(200, gin.H{
+		"Mensaje": "Entrada añadida con exito",
+		"Titulo": titulo,
+		"Dia": dia,
+		"Hora": hora,
+		"Entrada": entrada,
+	})
+}
+
+func obtenerEntrada(c *gin.Context){
+	
+	c.JSON(200, gin.H{
+		"Mensaje": "La entrada es: ",
+		"Titulo": new.GetTitulo(),
+		"Dia": new.GetDia(),
+		"Hora": new.GetHora(),
+		"Entrada": new.GetTexto(),
+	})
+}
+
 
 func introducirTitulo(c *gin.Context){
 
@@ -34,6 +63,9 @@ func main() {
 
 	r.POST("/anadetitulo", introducirTitulo)
 	r.GET("/obtenertitulo", obtenerTitulo)
+
+	r.POST("/anadeentrada", introducirEntrada)
+	r.GET("/obtenerEntrada", obtenerEntrada)
 
 	r.Run()
 }
